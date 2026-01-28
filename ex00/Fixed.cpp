996d8fdd02ff7d32d21ea1ff6000fe707e9ee4b2
dsemenov <dsemenov@student.42lyon.fr>
2026-01-28 20:47:14 +0100

    feat: implement all constructors

--- ex00/Fixed.cpp
+++ ex00/Fixed.cpp
@@ -1,25 +1,35 @@
+#include <iostream>
 #include "Fixed.hpp"
 
+Fixed::Fixed() : _value(0)
+{
+  std::cout << "Default constructor called\n";
+}
 
-Fixed::Fixed() : _value(0) {}
 Fixed::Fixed(const Fixed& other)
 {
+    std::cout << "Copy constructor called\n";
     *this = other;
 }
 
 Fixed&	Fixed::operator=(const Fixed& other)
 {
+  std::cout << "Copy assignment operator called\n";
   if (this != &other)
   {
-    _value = other._value;
+    this->_value = other.getRawBits();
   }
   return (*this);
 }
 
-Fixed::~Fixed() {}
+Fixed::~Fixed()
+{
+  std::cout << "Destructor called\n";
+}
 
 int Fixed::getRawBits() const
 {
+  std::cout << "getRawBits member function called\n";
   return (_value);
 }
 
